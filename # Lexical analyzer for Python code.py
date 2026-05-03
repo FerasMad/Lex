de124b570ddf6f03aba3ffffc5efd70ffc9e212d
AkiraKh98 <mjeedkh98@gmail.com>
2026-05-03 20:08:56 +0300

    2nd commit: Fully implemented methods of the compiler other than scanner and input methods

--- # Lexical analyzer for Python code.py
+++ # Lexical analyzer for Python code.py
@@ -78,3 +78,104 @@ def optional_nfa(nfa):
     epsilon_nfa = create_base_nfa(['epsilon'])
     return union_nfas(nfa, epsilon_nfa)
 
+def preprocess_regex(regex):
+    result = []
+
+    for i in range(len(regex)):
+        current_char = regex[i]
+        result.append(current_char)
+
+        if i + 1 < len(regex):
+            next_char = regex[i+1]
+            
+            left_valid = current_char not in ['(', '|']
+            right_valid = next_char not in ['*', '+', '?', '|', ')']
+
+            if right_valid and left_valid:
+                result.append('.')
+            
+    return ''.join(result)
+
+def infix_to_postfix(regex):
+    regex = preprocess_regex(regex)
+
+    output = []
+    stack = []
+    precedence = {'*' : 2, '+' : 2, '?' : 2, '.' : 1, '|' : 0}
+
+    for char in regex:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            while len(stack) > 0 and stack[-1] != '(':
+                output.append(stack.pop())
+            stack.pop()
+        elif char in precedence:
+            while len(stack) > 0 and stack[-1] != '(' and precedence[stack[-1]] >= precedence[char]:
+                output.append(stack.pop())
+            stack.append(char)
+        else:
+            output.append(char)
+        
+    while len(stack) > 0:
+        output.append(stack.pop())
+
+    return ''.join(output)
+
+
+def build_nfa(postfix):
+    stack = []
+
+    for char in postfix:
+        if char in ['*', '+', '?']:
+            nfa = stack.pop()
+            if char == '*':
+                stack.append(kleene_star_nfa(nfa))
+            elif char == '+':
+                stack.append(plus_nfa(nfa))
+            elif char == '?':
+                stack.append(optional_nfa(nfa))
+
+        elif char in ['.', '|']:
+            nfa2 = stack.pop()
+            nfa1 = stack.pop()
+
+            if char == '.':
+                stack.append(concatenate_nfas(nfa1, nfa2))
+            elif char == '|':
+                stack.append(union_nfas(nfa1, nfa2))
+
+        else:
+            stack.append(create_base_nfa([char]))
+    return stack.pop()
+
+
+def get_epsilon_closure(states):
+    closure = set(states) # Start by including the original states
+    stack = list(states)  # Use a stack to explore
+
+    # 1. While the stack has items:
+    while len(stack) > 0:
+        current_state = stack.pop()
+        if 'epsilon' in current_state.transitions:
+            for next_state in current_state.transitions['epsilon']:
+                if next_state not in closure:
+                    closure.add(next_state)
+                    stack.append(next_state)
+
+    return closure
+
+
+def evaluate(nfa, word):
+    start_state, end_state = nfa
+    current_states = get_epsilon_closure([start_state])
+
+    for char in word:
+        next_states = set()
+    
+        for state in current_states:
+            if char in state.transitions:
+                next_states.update(state.transitions[char])
+        current_states = get_epsilon_closure(list(next_states))
+
+    return end_state in current_states
